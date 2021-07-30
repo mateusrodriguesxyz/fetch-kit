2debef5847e31c51d220afd3cef98b5700aa0209
Mateus Rodrigues <mateus.fr26@gmail.com>
2021-07-30 09:49:54 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -25,6 +25,7 @@ let package = Package(
         ),
         .target(
             name: "FetchKitSwift",
+            path: "FetchKitSwift",
             dependencies: ["FetchKit"]
         )
     ]
